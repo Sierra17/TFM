d5b5ef48261432a36d60f322daadf8537ba6d1fd
Sierra17 <luissierramuntane17@gmail.com>
2024-04-10 15:43:26 +0200

    update main.py

--- Code/GraphMC/main.py
+++ Code/GraphMC/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import mc_solver
 import mc_run
 
@@ -5,4 +6,11 @@ gamma_n = 5
 gamma_r = 0.003
 gamma_c = 0.003
 
-mc_run(gamma_n, gamma_r, gamma_c)
+hparams = [gamma_n, gamma_r, gamma_c]
+
+# data matrix to be completed, alternatively provide mask
+X = np.array(input_matrix) 
+if np.isnan(X).any():
+    mask = np.isnan(X)
+
+mc_run(X, mask, hparams)
